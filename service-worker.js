7e992b628c430836331eca4d239f6b1ae429481d
ltc05588 <naysalapao44@gmail.com>
2026-08-02 17:35:05 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ltc-visit-cache-v1';
+const urlsToCache = [
+  '/ltc-visit-system/',
+  '/ltc-visit-system/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
